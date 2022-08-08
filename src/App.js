dbbe02d255f9d68ad50864811d7739a92c0dd03a
NF38 <93057555+NF38@users.noreply.github.com>
2022-08-08 17:54:00 +0300

    adding landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Landing from "./pages/Landing";
 const App = () => {
   return (
     <div>
-      <h1>Jobify</h1>
       <Landing />
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { ReactComponent as Logo } from "../assets/images/logo.svg";
-import { ReactComponent as Main } from "../assets/images/main.svg";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/job-offers.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
-    <div>
-      <h4>Landing Page</h4>
-    </div>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem laudantium expedita, illo asperiores non voluptatibus
+            magni veritatis tempora et est officiis! Praesentium sequi aperiam
+            labore reprehenderit magnam alias delectus! Provident!
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="Job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
