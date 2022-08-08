a435648ac0e91ce554e4d555cd4dad208ed660e6
NF38 <93057555+NF38@users.noreply.github.com>
2022-08-08 14:41:00 +0300

    setup mern stack jobify app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Landing from "./pages/Landing";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Jobify</h1>
+      <Landing />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Logo } from "../assets/images/logo.svg";
+import { ReactComponent as Main } from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <div>
+      <h4>Landing Page</h4>
+    </div>
+  );
+};
+
+export default Landing;
